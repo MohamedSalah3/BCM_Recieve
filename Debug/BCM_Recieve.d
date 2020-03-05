9b2bc6f52e88d38e72e269f0a92a4fac6c9fcf35
MohamedSalah3 <mhmd.ahmd789@gmail.com>
2020-03-05 03:02:31 +0200

    BRIDGE WORKING UART_SPI_INT

--- Debug/BCM_Recieve.d
+++ Debug/BCM_Recieve.d
@@ -1,1 +1,12 @@
-BCM_Recieve.d BCM_Recieve.o: .././BCM_Recieve.c
+BCM_Recieve.d BCM_Recieve.o: .././BCM_Recieve.c .././BCM_Recieve.h \
+ .././BCM_Config.h .././SPI.h .././std_types.h .././SPI_Config.h
+
+.././BCM_Recieve.h:
+
+.././BCM_Config.h:
+
+.././SPI.h:
+
+.././std_types.h:
+
+.././SPI_Config.h:
